6d96595659cf6929d07d90e993bf61abe77b914f
ranaagamaall <ranaagamaall@gmail.com>
2023-09-13 02:51:08 +0300

    change to area chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,34 +1,6 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts';
+import { XAxis, YAxis, Tooltip, ResponsiveContainer, BarChart, Bar, AreaChart, Area } from 'recharts';
 
 
-// export default function Chart(props) {
-//     console.log(props.data);
-//     return (
-//         <ResponsiveContainer width="80%" height="40%" >
-//             <BarChart
-//                 width="100%"
-//                 height="100%"
-//                 data={props.data}
-//             // margin={{
-//             //     top: 5,
-//             //     right: 30,
-//             //     left: 20,
-//             //     bottom: 5,
-//             // }}
-//             >
-//                 {/* <CartesianGrid strokeDasharray="3 3" /> */}
-//                 <XAxis dataKey={props.xKey} />
-//                 <YAxis domain={['dataMin', 'dataMax']} />
-//                 {/* open modal on hover */}
-//                 <Tooltip />
-//                 {/* <Legend /> */}
-//                 <Bar dataKey={props.ykey} fill="#000" />
-//                 {/* <Line type="monotone" dataKey={props.yKey} stroke="#8884d8" activeDot={{ r: 8 }} /> */}
-//                 {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-//             </BarChart>
-//         </ResponsiveContainer>
-//     );
-// }
 export default function Chart(props) {
 
 
@@ -45,12 +17,18 @@ export default function Chart(props) {
                         <Bar dataKey={props.yKey} fill="#8884d8" />
                     </BarChart> :
 
-                    <LineChart data={props.data}>
+                    <AreaChart data={props.data}>
+                        <defs>
+                            <linearGradient id="color1" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
                         <XAxis dataKey={props.xKey} />
                         <YAxis domain={['dataMin', 'dataMax']} />
                         <Tooltip labelStyle={{ color: "black" }} />
-                        <Line type="monotone" dataKey={props.yKey} stroke="#8884d8" activeDot={{ r: 8 }} />
-                    </LineChart>
+                        <Area type="monotone" dataKey={props.yKey} stroke="#8884d8" fillOpacity={1} fill="url(#color1)" activeDot={{ r: 8 }} />
+                    </AreaChart>
             }
         </ResponsiveContainer>
     );
