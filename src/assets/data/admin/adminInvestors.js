3fc10978bec974ab27c660acf35338b11b73d8cf
ranaagamaall <ranaagamaall@gmail.com>
2023-09-14 02:07:27 +0300

    admin investors add investments review

--- src/assets/data/admin/adminInvestors.js
+++ src/assets/data/admin/adminInvestors.js
@@ -8,46 +8,90 @@ const data = {
     ],
     users: [
         {
+            id: 1,
             name: 'John Doe',
             email: 'john@doe.com',
-            numberOfTransactions: 5,
-            totalInvestment: 10000,
-            currentBalance: 10000,
-            pic: 'https://source.unsplash.com/random/200x200'
+            numberOfTransactions: 8,
+            totalInvestment: 1000,
+            currentBalance: 20000,
+            pic: 'https://source.unsplash.com/random/200x200',
+            investments: [
+                {
+                    stock: 'AAPL',
+                    amount: 1000,
+                    price: 100,
+                    date: '2021-05-01'
+                },
+                {
+                    stock: 'Samsung',
+                    amount: 100,
+                    price: 900,
+                    date: '2021-05-04'
+                },
+                {
+                    stock: 'AAPL',
+                    amount: 1000,
+                    price: 100,
+                    date: '2021-05-01'
+                },
+                {
+                    stock: 'Samsung',
+                    amount: 100,
+                    price: 900,
+                    date: '2021-05-04'
+                },
+                {
+                    stock: 'AAPL',
+                    amount: 1000,
+                    price: 100,
+                    date: '2021-05-01'
+                },
+                {
+                    stock: 'Samsung',
+                    amount: 100,
+                    price: 900,
+                    date: '2021-05-04'
+                },
+            ],
         },
         {
+            id: 2,
             name: 'Rana Gamal',
             email: 'john@doe.com',
             numberOfTransactions: 5,
-            totalInvestment: 10000,
-            currentBalance: 10000,
+            totalInvestment: 4200,
+            currentBalance: 100,
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 3,
             name: 'John Gamal',
             email: 'john@doe.com',
-            numberOfTransactions: 5,
-            totalInvestment: 10000,
-            currentBalance: 10000,
+            numberOfTransactions: 9,
+            totalInvestment: 9000,
+            currentBalance: 4000,
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 4,
             name: 'Hi hello',
             email: 'john@doe.com',
             numberOfTransactions: 5,
             totalInvestment: 10000,
-            currentBalance: 10000,
+            currentBalance: 80000,
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 5,
             name: 'Test test',
             email: 'john@doe.com',
             numberOfTransactions: 5,
-            totalInvestment: 10000,
-            currentBalance: 10000,
+            totalInvestment: 2310,
+            currentBalance: 340,
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 6,
             name: 'John Doe',
             email: 'john@doe.com',
             numberOfTransactions: 5,
@@ -56,6 +100,7 @@ const data = {
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 7,
             name: 'John Doe',
             email: 'john@doe.com',
             numberOfTransactions: 5,
@@ -64,6 +109,7 @@ const data = {
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 8,
             name: 'John Doe',
             email: 'john@doe.com',
             numberOfTransactions: 5,
@@ -72,6 +118,7 @@ const data = {
             pic: 'https://source.unsplash.com/random/200x200'
         },
         {
+            id: 9,
             name: 'John Doe',
             email: 'john@doe.com',
             numberOfTransactions: 5,
--- src/pages/admin/investors/Investors.js
+++ src/pages/admin/investors/Investors.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import data from '../../../assets/data/admin/adminInvestors.js';
 
 
 function Investors() {
 
   const [users, setUsers] = useState(data.users);
+  const [userToInspect, setUserToInspect] = useState({});
+  const dialog = useRef(null);
+
+  function openInvestmentsModal(userId) {
+    setUserToInspect(data.users.find(user => user.id === userId))
+    dialog.current.showModal()
+  }
 
   return (
-    <div className="relative flex flex-col w-full min-w-0 mb-0 break-words bg-secondary border-0 border-transparent border-solid shadow-xl rounded-2xl bg-clip-border">
-      <div class="relative mt-2 w-1/2 mx-auto flex  flex-wrap items-stretch">
+    <div className="relative flex flex-col w-full min-w-0 mb-0 break-words border-0 border-transparent border-solid shadow-xl bg-secondary rounded-2xl bg-clip-border">
+      <div className="relative flex flex-wrap items-stretch w-1/2 mx-auto mt-2">
         <input
           type="search"
-          class="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-text bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-text outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-text focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
+          className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-text bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-text outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-text focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
           placeholder="Search"
           onChange={(e) => { setUsers(data.users.filter(user => user.name.toLowerCase().includes(e.target.value.toLowerCase()))) }}
         />
@@ -25,7 +32,7 @@ function Investors() {
                 {
                   data.columns.map((column, index) =>
 
-                    <th key={index} className="px-6 py-3 font-semibold font-main text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-text ">
+                    <th key={index} className="px-6 py-3 font-semibold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none font-main text-xxs border-b-solid tracking-none whitespace-nowrap text-text ">
                       {column}
                     </th>
                   )
@@ -50,26 +57,65 @@ function Investors() {
                   </td>
 
                   <td className="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-">
-                    <p className="mb-0 text-s font-medium leading-tight text-center font-main opacity-80">{user.numberOfTransactions}</p>
+                    <p className="mb-0 font-medium leading-tight text-center text-s font-main opacity-80">{user.numberOfTransactions}</p>
                   </td>
                   <td className="p-2 text-sm leading-normal text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                    <p className="mb-0 text-s font-medium leading-tight text-center font-main opacity-80">{user.totalInvestment}</p>
+                    <p className="mb-0 font-medium leading-tight text-center text-s font-main opacity-80">{user.totalInvestment}</p>
                   </td>
                   <td className="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                    <p className="mb-0 text-s font-medium leading-tight text-center font-main opacity-80">{user.currentBalance}</p>
+                    <p className="mb-0 font-medium leading-tight text-center text-s font-main opacity-80">{user.currentBalance}</p>
                   </td>
                   <td className="p-2 align-middle border-b whitespace-nowrap shadow-transparent">
-                    <a className="px-4 py-2 text-s font-semibold leading-tight rounded-lg bg-success text-primary font-main">view</a>
+                    <button onClick={() => openInvestmentsModal(user.id)}
+                      className="px-4 py-2 font-semibold leading-tight rounded-lg text-s bg-success text-primary font-main">view</button>
+
                   </td>
-
-
                 </tr>
               )
               }
             </tbody>
+
           </table>
         </div>
       </div>
+      <dialog ref={dialog} className="ml-[20%] w-4/5 max-h-full bg-transparent text-text">
+        <div className="w-4/5 m-auto bg-primary rounded-2xl">
+          <div className="flex items-start justify-between p-4 border-b rounded-t">
+            <div className="flex px-2 py-1">
+              <div>
+                <img src={userToInspect.pic} className="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl" alt="user" />
+              </div>
+              <div className="flex flex-col justify-center">
+                <h6 className="mb-0 text-sm leading-normal font-main">{userToInspect.name}</h6>
+                <p className="mb-0 text-xs leading-tight text-slate-400 font-main" >{userToInspect.email}</p>
+              </div>
+            </div>
+            <button onClick={() => dialog.current.close()}
+              className="inline-flex items-center justify-center w-8 h-8 ml-auto text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white" >
+              X
+            </button>
+          </div>
+          <div className="flex flex-col p-6">
+            {
+              userToInspect.investments.map((investment, index) =>
+                <div key={index} className="flex justify-between">
+                  <p className="text-sm font-medium ">{investment.stock}</p>
+                  <p className="text-sm font-medium ">{investment.amount}</p>
+                  <p className="text-sm font-medium ">{investment.price}</p>
+                  <p className="text-sm font-medium ">{investment.date}</p>
+                </div>
+              )
+            }
+          </div>
+          <div className="flex items-center justify-end p-6 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <button
+              onClick={() => dialog.current.close()}
+              className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+              Return
+            </button>
+          </div>
+        </div>
+      </dialog>
     </div>
 
   );
