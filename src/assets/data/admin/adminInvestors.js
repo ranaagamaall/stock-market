c78ca053ecd1922b14ce48bb6e6665eb857c58ea
ranaagamaall <ranaagamaall@gmail.com>
2023-09-13 03:52:58 +0300

    admin Investor initial

---
+++ src/assets/data/admin/adminInvestors.js
@@ -0,0 +1,141 @@
+const data = {
+    columns: [
+        "Investor",
+        "Transactions",
+        "Investment",
+        "Balance",
+        "",
+    ],
+    users: [
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'Rana Gamal',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Gamal',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'Hi hello',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'Test test',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'Rana Gamal',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Gamal',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'Hi hello',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'Test test',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            numberOfTransactions: 5,
+            totalInvestment: 10000,
+            currentBalance: 10000,
+            pic: 'https://source.unsplash.com/random/200x200'
+        },
+    ]
+};
+
+export default data;
