34f8290283b0c7edae631e1b034830f3c962f2a7
ranaagamaall <ranaagamaall@gmail.com>
2023-09-13 04:43:57 +0300

    adjust admin stocks

--- src/pages/admin/stocks/Stocks.js
+++ src/pages/admin/stocks/Stocks.js
@@ -4,7 +4,7 @@ function Stocks() {
   function handleSubmit(values) { }
   return (
     <div className="flex flex-wrap justify-evenly">
-      <div className="flex flex-col justify-center gap-6 text-center basis-1/2">
+      <div className="flex flex-col justify-center gap-6 text-center basis-[40%] min-w-[20rem]">
         <h3 className="text-3xl font-semibold uppercase text-accent font-main">
           Introduce stocks
         </h3>
@@ -13,7 +13,7 @@ function Stocks() {
         </p>
         <img className="w-3/5 mx-auto rounded-3xl" src="https://img.freepik.com/free-photo/stock-market-chart-technology-blue-background_53876-124650.jpg?w=1060&t=st=1694567619~exp=1694568219~hmac=9ce454bf5dc5931019b322e2e30f44530ce14bd69ec066465c9d92927b806778" />
       </div>
-      <div className="px-12 basis-4/6">
+      <div className="px-12 basis-[60%] min-w-[20rem]">
         <StockForm />
       </div>
     </div>
